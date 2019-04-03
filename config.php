707ba1c5ac2d00239da57595f99d6ab404ae49b1
rus_alex <me@alexandru.live>
2019-04-03 19:43:11 +0300

    addded config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "echipa5.cio39zzweoyk.eu-west-1.rds.amazonaws.com";
+$username = "admin";
+$password = "nuav3Msef";
+$db_name = "echipa5";
+$tbl_name_authors = "authors";
+$tbl_name_books = "books";
+$tbl_name_book_authors = "books_authors";
+$tbl_name_book_categories = "book_categories";
+$tbl_name_categories = "categories";
+$tbl_name_categories = "categories";
+$tbl_name_publishers = "publishers";
+$tbl_name_reviews = "reviews";
+$tbl_name_user_roles = "user_roles";
+$tbl_name_users = "users";
+?>
--- search.php
+++ search.php
@@ -3,9 +3,53 @@
 <head>
     <title>Search</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<p> Adding something here!!!</p>
 </head>
 <body>
  
+<h1>This wonderful search page is done. Almost ... ish </h1>
+
+<form action="search.php" method="GET">
+    <input type="text" name="query" />
+    <input type="submit" value="Search" />
+</form>
+
+
+
+
+
+<?php
+
+if (isset($_SESSION['Username']))
+  {
+    require_once ("config.php");
+    $orderby = $_SESSION['Orderby'];
+    $db=mysqli_connect($servername,$username,$password,$db_name) or die ("could not connect");
+    $query="SELECT * FROM $tbl_name ORDER BY username $orderby";
+    $result=mysqli_query($db,$query);
+    while ($r=mysqli_fetch_array($result)) {
+        $user4=$r['username'];
+        echo "<div class='row'>";
+        echo "<div class='column left'><p align='left'>".$user4."</p></div>";
+        $nume4=$r['nume'];
+        $prenume4=$r['prenume'];
+        echo "<div class='column middle'><p align='left'>".$nume4." ".$prenume4."</p></div>";
+        $id4=$r['id'];
+        echo "<div class='column right'><p align='left'><a href='utilizatori.php?id=$id4' target='_self'>Mai multe detalii</a></p></div>";
+        echo"</div>";
+      }
+  }
+
+else
+  {
+    require_once ("config.php");
+    $db=mysqli_connect($host,$usernamesql,$passwordsql,$db_name) or die ("could not connect");
+    $query="SELECT * FROM $tbl_name";
+    $result=mysqli_query($db,$query);
+    while ($r=mysqli_fetch_array($result)) {
+        $user4=$r['username'];
+        echo "<p align='left'>".$user4."</p>";
+      }
+    }
+?>
 </body>
 </html>
