8f93a60138647d7fc16918adeb9a6b67c69c6b2e
Alexandru Ciprian Rus <me@alexandru.live>
2019-04-09 23:58:24 +0300

    functionality of search by book name and title are done, WIP : search by category

--- config.php
+++ config.php
@@ -3,11 +3,11 @@ $servername = "echipa5.cio39zzweoyk.eu-west-1.rds.amazonaws.com";
 $username = "admin";
 $password = "nuav3Msef";
 $db_name = "echipa5";
+
 $tbl_name_authors = "authors";
 $tbl_name_books = "books";
 $tbl_name_book_authors = "books_authors";
 $tbl_name_book_categories = "book_categories";
-$tbl_name_categories = "categories";
 $tbl_name_categories = "categories";
 $tbl_name_publishers = "publishers";
 $tbl_name_reviews = "reviews";
