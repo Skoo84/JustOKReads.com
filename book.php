02a12532a155c4d552bff73365554bf1adeb1b88
Sebastian Boar <boar.sebastian@gmail.com>
2019-04-30 15:14:30 +0300

    voting system

--- book.php
+++ book.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Book</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf8_unicode_ci" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8-unicode-ci" />
 </head>
 <body>
  
@@ -77,5 +77,42 @@ if(isset($_GET['bookid']))
 
   } 
 ?>
+<div class="row">
+<div class="col-sm-12">
+<form id="ratingForm" method="POST">
+<div class="form-group">
+<h4>Rate this product</h4>
+<button type="button" class="btn btn-warning btn-sm rateButton" aria-label="Left Align">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+</button>
+<button type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+</button>
+<button type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+</button>
+<button type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+</button>
+<button type="button" class="btn btn-default btn-grey btn-sm rateButton" aria-label="Left Align">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+</button>
+<input type="hidden" class="form-control" id="rating" name="rating" value="1">
+<input type="hidden" class="form-control" id="itemId" name="itemId" value="12345678">
+</div>
+<div class="form-group">
+<label for="usr">Title*</label>
+<input type="text" class="form-control" id="title" name="title" required>
+</div>
+<div class="form-group">
+<label for="comment">Comment*</label>
+<textarea class="form-control" rows="5" id="comment" name="comment" required></textarea>
+</div>
+<div class="form-group">
+<button type="submit" class="btn btn-info" id="saveReview">Save Review</button> <button type="button" class="btn btn-info" id="cancelReview">Cancel</button>
+</div>
+</form>
+</div>
+</div>
 </body>
 </html>
