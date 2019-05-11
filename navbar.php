8b188547fdbe54106ce7ef1815202201b3382b78
boar.sebastian@gmail.com <Mehedinti1>
2019-05-11 16:57:35 +0000

    logo name

--- navbar.php
+++ navbar.php
@@ -73,7 +73,7 @@ body {
 
 <div class="navbar">
 <a href="index.php" class="active">Home</a>
-  <img src="Logo.png" alt="JustOKReads.com" style="width:151px;border:0;">
+  <img src="logo.png" alt="JustOKReads.com" style="width:151px;border:0;">
 </a>
   <a href="search.php">Search</a>
   <a href="login.php">Login</a>
