21bbba3f93bb356698c1eb1101f47648d63c5c5e
Alexandru Rus <mail@arus.ro>
2019-05-11 16:48:09 +0300

    edited createUser from Ro to English - WIP

--- createUser.php
+++ createUser.php
@@ -10,9 +10,9 @@
 <?php
  session_start();
 // definirea valorilor pentru erori ca fiind goale. Altfel la prima afisare a formularului ar da o eroare
-$numeerr=$emailerr=$passworderr=$usernameerr="";
+$usrnameerr=$emailerr=$passworderr=$usernameerr="";
 // definirea variabilelor pentru valorile din formular. Daca nu am stabili, la prima afisare a formularului ar da eroare
-$birthDate=$roleId=$about=$nume1=$passwordUser=$email1=$username1="";
+$birthDate=$roleId=$about=$usrname1=$passwordUser=$email1=$username1="";
 //definirea valorii la eroarea principala. Daca intervine orice eroare la verificare formular, ii vom atribui o alta valoare
 $eroare=0;
 //se deschide fisierul de configurare, pentru a citi variabilele definite de noi
@@ -28,22 +28,22 @@ function test_input($data)
 // verifica daca au fost trimise date din formular
 if ($_SERVER["REQUEST_METHOD"]=="POST")
   {
-// verifica daca campul nume a fost completat cu functia empty
-    if (empty($_POST["nume"]))
+// verifica daca campul usrname a fost completat cu functia empty
+    if (empty($_POST["usrname"]))
       {
 // in cazul in care nu a fost completat defineste o eroare
-        $numeerr="Numele este obligatoriu";
+        $usrnameerr="usrnamele este obligatoriu";
 // in cazul in care a fost definit o eroare la verificarea formularului, se modifica si eroarea principala
         $eroare=1;
       }
     else
       {
-// se modifica valoarea goala de la variabila $nume cu valoarea primita din formular
-        $nume1=test_input($_POST["nume"]);
+// se modifica valoarea goala de la variabila $usrname cu valoarea primita din formular
+        $usrname1=test_input($_POST["usrname"]);
 // daca campul a fost completat, se verifica daca contine numai litere si spatii folosind functia de filtrare preg_match
-        if (!preg_match("/^[a-zA-Z ]*$/",$nume1))
+        if (!preg_match("/^[a-zA-Z ]*$/",$usrname1))
           {
-            $numeerr="Numai litere si spatii sunt acceptate";
+            $usrnameerr="Numai litere si spatii sunt acceptate";
             $eroare=1;
           }
       }
@@ -65,13 +65,13 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
           }
         if (empty($_POST["username"]))
           {
-            $usernameerr="Utilizatorul este obligatoriu";
+            $usernameerr="Usernameul este obligatoriu";
             $eroare=1;
           }
         else
           {
             $username1=test_input($_POST["username"]);
-// daca campul a fost completat, se verifica daca contine numai litere, numere si spatii
+// daca campul a fost completat, se verifica daca contine numai litere, usrnamere si spatii
             if (!preg_match("/^[a-zA-Z0-9 ]*$/",$username1))
               {
                 $usernameerr="Numai litere, cifre si spatii sunt acceptate";
@@ -79,24 +79,24 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
               }
           }
             
-          if (empty($_POST["parolaa"]))
+          if (empty($_POST["usrPassworda"]))
             {
-              $passworderr="Parola este obligatorie";
+              $passworderr="usrPassword este obligatorie";
               $eroare=1;
             }
           else
             {
-              if (empty($_POST["parolab"]))
+              if (empty($_POST["usrPasswordb"]))
                 {
-                  $passworderr="Parola este obligatorie";
+                  $passworderr="usrPassword este obligatorie";
                   $eroare=1;
                 }
                 else
                   {
 // se verefica daca cele doua parole sunt identice
-                    if ($_POST["parolaa"]!=$_POST["parolab"])
+                    if ($_POST["usrPassworda"]!=$_POST["usrPasswordb"])
                       {
-                        $passworderr="Parola nu se potriveste";
+                        $passworderr="usrPassword nu se potriveste";
                         $eroare=1;
                       }
                   }
@@ -118,7 +118,7 @@ echo "<p>Numarul de erori este ".$eroare."</p>";
          $num=mysqli_num_rows($result);
          if($num==1)
          {
-           $usernameerr="Utilizator existent. Alegeti alt utilizator";
+           $usernameerr="Username existent. Alegeti alt Username";
            $eroare=1;
          }
           $query="SELECT * FROM $tbl_name_users WHERE email='$email1'";
@@ -142,22 +142,20 @@ echo "<p>Numarul de erori este ".$eroare."</p>";
            $result=mysqli_query($db,$query);
            $result2=mysqli_fetch_array($result);
            $idnew=$result2['max']+1;
-// se sanitizeaza parola
-           $parola1=test_input($_POST["parolaa"]);
+// se sanitizeaza usrPassword
+           $usrPassword1=test_input($_POST["usrPassworda"]);
 // se sanitizeaza about
            $about1=test_input($_POST["about"]);
-// se sanitizeaza numele utilizatorului
-            $nume1=test_input($_POST["nume"]);
-               // se sanitizeaza data de nastere utilizatorului
+// se sanitizeaza usrnamele Usernameului
+            $usrname1=test_input($_POST["usrname"]);
+               // se sanitizeaza data de nastere Usernameului
             $birthDate=test_input($_POST["birthDate"]);     
-// se cripteaza parola
-           $parola1=md5($parola1);
+// se cripteaza usrPassword
+           $usrPassword1=md5($usrPassword1);
 
 // inserare in baza de date
-            echo "<p> TRIMITEM DATELE LA BAZA DE DATE</p>";
-            
 
-            $query="INSERT INTO $tbl_name_users (id, name, birth_date, username, email, password, role_id, about) VALUES ('$idnew', '$nume1', '$birthDate', '$username1', '$email1', '$parola1', '5','$about1')";
+            $query="INSERT INTO $tbl_name_users (id, name, birth_date, username, email, password, role_id, about) VALUES ('$idnew', '$usrname1', '$birthDate', '$username1', '$email1', '$usrPassword1', '5','$about1')";
             $result=mysqli_query($db,$query);
           
 // se specifica pagina catre care se va redirectiona dupa inserare in baza de date
@@ -165,38 +163,40 @@ echo "<p>Numarul de erori este ".$eroare."</p>";
           }
   }
 ?>
-<h2>Inregistrare utilizator nou</h2>
+<h2>Inregistrare Username nou</h2>
 <p><span class="error">* camp obligatoriu</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" target="_self" enctype="multipart/form-data">
   <fieldset>
-  <legend>Informatii personale</legend>
-    Nume: <br><input type="text" name="nume" value="<?php echo $nume1; ?>" lenght="80">
-    <span class="error">* <?php echo $numeerr;?></span><br><br>
+  <legend>Personal user informations</legend>
+  <br>
+    Name of the User: <br><input type="text" name="usrname" value="<?php echo $usrname1; ?>" lenght="80">
+    <span class="error">* <?php echo $usrnameerr;?></span><br><br>
     E-mail: <br><input type="text" name="email" value="<?php echo $email1; ?>" lenght="80">
     <span class="error">* <?php echo $emailerr;?></span><br><br>
     Birth Date: <br><input type="date" name="birthDate" min="1900-01-01"value="<?php echo $birthDate; ?>" lenght=80">
     <br>    <br>
     About: <br><input type="text" name="about" value="<?php echo $about; ?>" lenght="240">
-    <br>    <br>
+    <br>    
     
   </fieldset>
   <br>
  
   <br>
   <fieldset>
-  <legend>Informatii login</legend>
-    Utilizator: <br><input type="text" name="username" value="<?php echo $username1; ?>" lenght="40">
+  <legend>Login Details</legend>
+  <br>
+    Username: <br><input type="text" name="username" value="<?php echo $username1; ?>" lenght="40">
     <span class="error">* <?php echo $usernameerr;?></span><br><br>
-    Parola: <br><input type="password" name="parolaa" lenght="40">
+    Password: <br><input type="password" name="usrPassworda" lenght="40">
     <br><br>
-    Verificare parola: <br><input type="password" name="parolab" lenght="40">
+    Repeat Password: <br><input type="password" name="usrPasswordb" lenght="40">
     <span class="error">* <?php echo $passworderr;?></span><br>
   </fieldset>
   <br>
-    <input type="submit" name="submit" value="Trimite">
+    <input type="submit" name="submit" value="Submit">
 </form>
 <br><br>
-<a href='index.php' target='_self'>Prima pagina</a>
+<a href='index.php' target='_self'>Home Page</a>
 &nbsp; | &nbsp;
 <a href='login.php' target='_self'>Login</a>
 </body></html>
