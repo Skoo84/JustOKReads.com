494ae5d69a97bd613ab43407bd08b606be21efee
boar.sebastian@gmail.com <Mehedinti1>
2019-05-18 15:58:44 +0000

    commit ma !!!

--- author.php
+++ author.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Search</title>
+    <title>Author</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="navbar.css">
 </head>
@@ -40,7 +40,7 @@ if(isset($_GET['authorid']))
 
 
     $db=mysqli_connect($servername,$username,$password,$db_name) or die ("could not connect");
-    $query="SELECT a.id, a.name, a.bio, a.picture, b.id, b.title, b.publisher_id, b.release_date,b.cover_photo,b.description,p.name
+    $query="SELECT a.id, a.name, a.bio, a.picture, b.id, b.title, b.publisher_id, b.release_date,b.cover_photo,b.description,p.name as pubname
     FROM authors a
     INNER JOIN books_authors ba ON ba.author_id=a.id AND a.id='$idAuthor'
     INNER JOIN books b ON ba.book_id=b.id
@@ -54,14 +54,14 @@ if(isset($_GET['authorid']))
     if ($result->num_rows > 0){
     while ($r=mysqli_fetch_array($result)) {
         $publisherid=$r['publisher_id'];
-        $publishername=$r['p.name'];
+        $publishername=$r['pubname'];
 
         $bookTitle=$r['title'];
         echo "<p align='left'> Book Name: ".$bookTitle."</p>";
         $publisherID=$r['publisher_id'];
         echo "<p align='left'> Publisher ID: ".$publisherid."</p>";
         
-        echo "<p align='left'> Publisher ID: ".$publishername."</p>";
+        echo "<p align='left'> Publisher : ".$publishername."</p>";
 
         echo "<p align='left'> ------------------------------</p>";
       }
