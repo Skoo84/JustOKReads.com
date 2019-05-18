717753892e1a7618f81af340751c085d12ad2960
Alexandru Rus <mail@arus.ro>
2019-05-18 15:28:21 +0300

    + userProfilePage edited to be functional

--- userprofilepage.php
+++ userprofilepage.php
@@ -2,31 +2,32 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title><?php echo $name; ?>'s Profile </title>
+    <title><?php session_start(); echo $_SESSION['Username']?>'s Profile</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf8-unicode-ci"/>
     <link rel="stylesheet" href="navbar.css">
+    <?php include('navbar.php'); ?>
 </head>
 <body>
+<div class="main2"><h1>Welcome <?php echo $_SESSION['Username']?></h1></div>
 <div>
 <?php
+
 // get the user details from data base
-if (isset($_GET['userid'])) {
+if (isset($_SESSION['UserID'])) {
     require_once("config.php");
     $db = new mysqli($servername, $username, $password, $db_name);
-
 // Check connection
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
     }
 
 // END of connection to DB
-    $idUser = $_GET['userid'];
+    $idUser = $_SESSION['UserID'];
 
     $sql = "SELECT * FROM $tbl_name_users WHERE id = '$idUser'";
     $result = mysqli_query($db, $sql);
     if ($result->num_rows > 0) {
         while ($row = mysqli_fetch_array($result)) {
-            echo "<h3>Hello, " . $row['name'] . '</br></h3>';
             echo '<table>';
             echo '<tr><td>Name:</td><td>' . $row["username"] . '</td></tr>';
             echo '<tr><td>Birth date:</td><td>' . $row["birth_date"] . '</td></tr>';
@@ -34,13 +35,13 @@ if (isset($_GET['userid'])) {
             echo '<tr><td>Description:</td><td>' . $row["about"] . '</td></tr>';
         }
     }
-    $db->close();
+    // $db->close(); Nu uita sa nu inchizi conexiuni cat timp mai ai nevoie de ele @Iulia.
 
 // get the user rewiews from data base
     $sql1 = "SELECT rw.description, rw.rating, rw.title
     FROM reviews rw
     JOIN users u ON rw.user_id = u.id
-    WHERE u.id = '$idUser'";
+    WHERE u.id = $idUser";
     $result1 = mysqli_query($db, $sql1);
     if ($result1->num_rows > 0) {
         if ($row = $result1->fetch_assoc()) {
@@ -49,10 +50,12 @@ if (isset($_GET['userid'])) {
             echo '<tr><td>Rating:</td><td>' . $row["rating"] . '</td></tr>';
             echo '<tr><td>Description:</td><td>' . $row["description"] . '</td></tr>';
             '</br>';
+            echo '</table>';
         }
-        echo '</table>';
     } else {
-        echo "0 results";
+        echo '<p>';
+        echo "There aren't any reviews to be displayed.";
+        echo '</p>';
     }
     $db->close();
 }
