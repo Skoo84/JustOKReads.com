04794c1b2bf203986d724b16f1e068cfb6630152
Sebastian Boar <boar.sebastian@gmail.com>
2019-05-22 15:47:51 +0300

    forum

--- codoforum.v.4.7.0/sites/default/plugins/FreiChat/FreiChat.info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$info['name'] = 'FreiChat';
-$info['description'] = 'Provides option to integrate with FreiChat';
-$info['version'] = '2.x';
-$info['author'] = "Codologic";
-$info['author_url'] = 'https://freichat.com';
-$info['license'] = 'Codologic License';
-$info['core'] = '4.x';
