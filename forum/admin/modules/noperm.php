1338aae078ab85c9d91facddb9f3648e1be7bba0
Sebastian Boar <boar.sebastian@gmail.com>
2019-05-22 15:55:29 +0300

    forum

--- forum/admin/modules/noperm.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$smarty = \CODOF\Smarty\Single::get_instance();
-$smarty->assign('msg', 'You do not have enough permissions.');
-
-global $CONF;
-
-$content = $smarty->fetch('noperm.tpl');
-
--- forum/sites/default/plugins/adv_role/adv_role.info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$info['name'] = 'Advanced Role system';
-$info['description'] = 'Provides more settings to configure roles/groups';
-$info['version'] = '1.x';
-$info['author'] = "Codologic";
-$info['author_url'] = 'https://codoforum.com';
-$info['license'] = 'GNU GPL V3';
-$info['core'] = '1.x';
