b4e81ae73933e61e1fdce9c3b023b2a7cff5d808
Sebastian Boar <boar.sebastian@gmail.com>
2019-05-22 11:33:15 +0300

    captcha code fix

--- constant.php
+++ constant.php
@@ -1,5 +1,5 @@
 <?php
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"6Len1qQUAAAAALvvPI-T2qa488G5AG0nEzTcVBXK"); 
-define('SECRET_KEY',"6Len1qQUAAAAAIWDwqsXgqZedljHyF7FG3cuP6IA")
+define('SITE_KEY',"6Lcq2aQUAAAAAO1budLdbfmZ41YnrAXb0KOfQRBA"); 
+define('SECRET_KEY',"6Lcq2aQUAAAAAK6J0lZrgowZi38SzQSw1M05LaTM")
 ?>
