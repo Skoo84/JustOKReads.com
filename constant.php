818b27e4d3ab1acd33ac114e5dbaf7cb5fcf863d
Sebastian Boar <boar.sebastian@gmail.com>
2019-05-22 11:24:24 +0300

    contact page test

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6Len1qQUAAAAALvvPI-T2qa488G5AG0nEzTcVBXK"); 
+define('SECRET_KEY',"6Len1qQUAAAAAIWDwqsXgqZedljHyF7FG3cuP6IA")
+?>
